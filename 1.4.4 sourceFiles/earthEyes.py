eed2638cb3d361a7290b9133d5cd67210fc39494
Jenna Titus <200675@shakopeeschools.org>
2018-03-06 13:56:44 -0600

    deleted info on file

--- 1.4.4 sourceFiles/earthEyes.py
+++ 1.4.4 sourceFiles/earthEyes.py
@@ -1,3 +1,8 @@
+'''earthEyes demonstrates PIL.Image.paste()
+Unpublished work (c)2013 Project Lead The Way
+CSE Activity 1.3.7 PIL API
+Version 9/10/2013 '''
+
 import PIL
 import matplotlib.pyplot as plt
 import os.path              
@@ -14,9 +19,8 @@ axes[0].imshow(student_img, interpolation='none')
 # Display student in second axes and set window to the right eye
 axes[1].imshow(student_img, interpolation='none')
 axes[1].set_xticks(range(1050, 1410, 100))
-axes[1].set_xlim(1050, 1400) #coordinat
+axes[1].set_xlim(1050, 1400) #coordinates measured in plt, and tried in iPython
 axes[1].set_ylim(1100, 850)
-fig.show()
 
 # Open, resize, and display earth
 earth_file = os.path.join(directory, 'earth.png')
@@ -25,17 +29,11 @@ earth_small = earth_img.resize((89, 87)) #eye width and height measured in plt
 fig2, axes2 = plt.subplots(1, 2)
 axes2[0].imshow(earth_img)
 axes2[1].imshow(earth_small)
-fig2.show()
 
 # Paste earth into right eye and display
 # Uses alpha from mask
-student_img.paste(earth_small, (1162, 966), mask=earth_small)
-student_img.paste(earth_small, (710, 945), mask=earth_small) 
- 
+student_img.paste(earth_small, (1162, 966), mask=earth_small) 
 # Display
 fig3, axes3 = plt.subplots(1, 2)
 axes3[0].imshow(student_img, interpolation='none')
-axes3[1].imshow(student_img, interpolation='none')
-axes3[1].set_xlim(500, 1500)
-axes3[1].set_ylim(1130, 850)
 fig3.show()
