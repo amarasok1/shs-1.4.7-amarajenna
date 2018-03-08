ea7bf13015e9b72833aebc7a73be841a50ab2e54
Jenna Titus <200675@shakopeeschools.org>
2018-03-08 13:02:31 -0600

    idk

--- 1.4.4 sourceFiles/addiconV1.py
+++ 1.4.4 sourceFiles/addiconV1.py
@@ -25,14 +25,14 @@ axes[1].set_ylim(200, 150)
 # Open, resize, and display earth
 yin_file = os.path.join(directory, 'yinyang.png')
 yin_img = PIL.Image.open(yin_file)
-yin_small = yinyang_img.resize((31, 31)) #eye width and height measured in plt
+yin_small = yin_img.resize((31, 31)) #eye width and height measured in plt
 fig2, axes2 = plt.subplots(1, 2)
 axes2[0].imshow(yin_img)
 axes2[1].imshow(yin_small)
 
 # Paste earth into right eye and display
 # Uses alpha from mask
-student_img.paste(yin_small, (109, 159), mask=earth_small) 
+student_img.paste(yin_small, (150,200), mask=yin_small) 
 # Display
 fig3, axes3 = plt.subplots(1, 2)
 axes3[0].imshow(student_img, interpolation='none')
