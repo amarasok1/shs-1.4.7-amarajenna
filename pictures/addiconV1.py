fd931b3688b645a92c4b79b98ce2fd6df05be70f
Jenna Titus <200675@shakopeeschools.org>
2018-03-11 23:13:52 -0500

    did things

--- pictures/addiconV1.py
+++ pictures/addiconV1.py
@@ -7,6 +7,24 @@ import PIL
 import matplotlib.pyplot as plt
 import os.path              
       
+def icon(directory=None):
+    if directory == None:
+        directory = os.getcwd() # Use working directory if unspecified
+        
+    image_list = [] # Initialize aggregaotrs
+    file_list = []
+    
+    directory_list = os.listdir(directory) # Get list of files
+    for entry in directory_list:
+        absolute_filename = os.path.join(directory, entry)
+        try:
+            image = PIL.Image.open(absolute_filename)
+            file_list += [entry]
+            image_list += [image]
+        except IOError:
+            pass # do nothing with errors tying to open non-images
+    return image_list, file_list
+    
 # Open the files in the same directory as the Python script
 directory = os.path.dirname(os.path.abspath(__file__))  
 student_file = os.path.join(directory, 'mainhardt.jpg')
@@ -34,6 +52,6 @@ axes2[1].imshow(yin_small)
 # Uses alpha from mask
 student_img.paste(yin_small, (150,200), mask=yin_small) 
 # Display
-fig3, axes3 = plt.subplots(1)
+fig3, axes3 = plt.subplots(1,2)
 axes3[0].imshow(student_img, interpolation='none')
 fig3.show()
